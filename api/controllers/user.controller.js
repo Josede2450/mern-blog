5ee4fe1974c58aae34e680c654e349491ca6faf0
Josede2450 <82405137+Josede2450@users.noreply.github.com>
2024-04-10 02:16:05 -0400

    Create a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,5 @@
+//Controller is used for manage the logic
+
+export const test = (req, res) => {
+  res.json({ message: "API is working" });
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { mongoose } from "mongoose"; //Import data base
 //nodemon works like a liveserver, we don't need to stop and start the program again, for see the results
 import dotenv from "dotenv"; //We Install and use dotenv for use enviroment variable
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config(); //For use env in our program
 mongoose
@@ -18,3 +19,5 @@ const app = express(); //Creating the app
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes); //That work for test our API
