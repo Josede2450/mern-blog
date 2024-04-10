e7b93e953300d3726095a42a54b9b31da2f17e5e
Josede2450 <82405137+Josede2450@users.noreply.github.com>
2024-04-10 01:47:40 -0400

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express";
-
+import { mongoose } from "mongoose"; //Import data base
 //nodemon works like a liveserver, we don't need to stop and start the program again, for see the results
+import dotenv from "dotenv"; //We Install and use dotenv for use enviroment variable
+
+dotenv.config(); //For use env in our program
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected"); // Connecting to the database
+  })
+  .catch((err) => {
+    console.log(err);
+  }); //If something goes wrong, printb the error
 
 const app = express(); //Creating the app
 
