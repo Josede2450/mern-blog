e946e1d6f5b28d2ae32c193bfd701db17f42cfb6
Josede2450 <82405137+Josede2450@users.noreply.github.com>
2024-04-11 00:49:50 -0400

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -27,6 +28,36 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json("Signup successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  //Validating the user in signin
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required")); //Handle the error, the logic is on utils
+  }
+
+  try {
+    const validUser = await User.findOne({ email }); //Validating the email
+    if (!validUser) {
+      next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password); //Validating the password
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc; //deleting the password from database
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true }) //Cookie
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup); // We send the logic to controller, and call the logic as a test
+router.post("/signin", signin);
 
 export default router;
