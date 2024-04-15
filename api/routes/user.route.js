b9a0951e9d376ad625d3e1903579a74caa3aee92
Josede2450 <82405137+Josede2450@users.noreply.github.com>
2024-04-15 19:09:43 -0400

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import {
+  deleteUser,
+  test,
+  updateUser,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test); // We send the logic to controller, and call the logic as a test
 router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser); //For delete the user
 
 export default router;
