a58639ff78a41227f7906567436ae775dae803e2
Josede2450 <82405137+Josede2450@users.noreply.github.com>
2024-04-16 22:56:41 -0400

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv"; //We Install and use dotenv for use enviroment vari
 import userRoutes from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser"; // For see the cookies
+import postRoutes from "./routes/post.route.js";
 
 dotenv.config(); //For use env in our program
 mongoose
@@ -27,6 +28,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes); //That work for test our API
 app.use("/api/auth", authRoute); //That work for signup our API
+app.use("/api/post", postRoutes);
 
 app.use((err, req, res, next) => {
   //Middleware
