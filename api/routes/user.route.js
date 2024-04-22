e1780f6ba0c769e7f37afcb0f1748ca6e4e1f10b
Josede2450 <82405137+Josede2450@users.noreply.github.com>
2024-04-22 16:19:13 -0400

    show the comment of a post

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,6 +5,7 @@ import {
   updateUser,
   signout,
   getUsers,
+  getUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -15,5 +16,6 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser); //For delete the user
 router.post("/signout", signout); // Signout route
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 
 export default router;
